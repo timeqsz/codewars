85e828aad4708b938f1db3535e61066030fec4d1
timeqsz <freed7333@gmail.com>
2023-04-20 01:50:01 +0800

    change interp.hs

--- haskell/interp.hs
+++ haskell/interp.hs
@@ -3,5 +3,5 @@
 -- 6 kyu
 -- 04 / 20 / 2023
 interp :: (Double -> Double) -> Double -> Double -> Int -> [Double]
-interp f l u n = map (\n -> (fromInteger $ floor $ (f n) * 100) / 100) (init [l, (l+diff) .. u])
+interp f l u n = map (\n -> fromInteger (floor $ f n * 100) / 100) (init [l, (l+diff) .. u])
          where diff = (u - l) / fromIntegral n
