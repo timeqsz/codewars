7b6868ddd708a549a9f4c53ab0c19f9110e83bc5
timeqsz <freed7333@gmail.com>
2023-04-20 01:38:51 +0800

    6 kyu 2023 / 04 / 20

---
+++ haskell/interp.hs
@@ -0,0 +1,7 @@
+
+-- Floating-point Approximation (II)
+-- 6 kyu
+-- 04 / 20 / 2023
+interp :: (Double -> Double) -> Double -> Double -> Int -> [Double]
+interp f l u n = map (\n -> (fromInteger $ floor $ (f n) * 100) / 100) (init [l, (l+diff) .. u])
+         where diff = (u - l) / fromIntegral n
